b5ada294d6bc804a8a2bd96016a541bbe9a75eb9
hassan kiwan <hassanhitleap1@gmail.com>
2021-06-27 11:57:17 +0300

    21

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,11 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('type')->nullable();
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_06_27_084459_create_shipments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipments', function (Blueprint $table) {
+            $table->id();
+            $table->string('policy_number');
+            $table->integer('driver_id');
+            $table->integer('customer_id');
+            $table->integer('areas_id');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('required_amount');
+            $table->float('delivery_amount');
+            $table->text('note');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipments');
+    }
+}
