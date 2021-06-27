8e714608d542fa044c640c3687b2c2243b88f626
hassan kiwan <hassanhitleap1@gmail.com>
2021-06-27 12:07:18 +0300

    30

---
+++ app/Http/Controllers/Users/DriverController.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Http\Controllers\Users;
+
+
+use App\Http\Controllers\Controller;
+
+class DriverController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+
+
+    public function delete(){
+
+    }
+}
