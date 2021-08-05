4ff7b7fe1a24ebac69cedd539a15501fe260597d
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-05 17:19:51 +0300

    15

--- app/Http/Controllers/Shipments/DelayController.php
+++ app/Http/Controllers/Shipments/DelayController.php
@@ -7,24 +7,30 @@ namespace App\Http\Controllers\Shipments;
 class DelayController
 {
     public function index(){
-
+        return DelayResource::collection(Shipment::all());
     }
 
-    public function create(){
-
+    public function create(Request $request){
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        Delay::cerate($request->all());
     }
 
     public function edit(){
 
     }
 
-    public function update(){
-
+    public function update(Delay $delay, Request $request){
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $delay->update([$request->all()]);
     }
 
 
-    public function delete(){
-
+    public function delete(Request $delay){
+        $delay->delete();
     }
 }
 
