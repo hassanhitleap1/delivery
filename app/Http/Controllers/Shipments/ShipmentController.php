98806d55467609bca48eb99914cb2736130a857d
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-05 17:05:57 +0300

    14

--- app/Http/Controllers/Shipments/ShipmentController.php
+++ app/Http/Controllers/Shipments/ShipmentController.php
@@ -4,26 +4,34 @@
 namespace App\Http\Controllers\Shipments;
 
 
+use App\Model\Shipments\Shipment;
+
 class ShipmentController
 {
     public function index(){
-
+        return ShipmentsResource::collection(Shipment::all());
     }
 
-    public function create(){
-
+    public function create(Request $request){
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        Shipment::cerate($request->all());
     }
 
     public function edit(){
 
     }
 
-    public function update(){
-
+    public function update(Shipment $shipment, Request $request){
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+        $shipment->update([$request->all()]);
     }
 
 
-    public function delete(){
-
+    public function delete(Request $shipment){
+        $shipment->delete();
     }
 }
