3d9392f62ecbcd4ed543843717adf397f0e42600
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-09 12:30:46 +0300

    27

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,19 @@
 use Illuminate\Support\Facades\Route;
 
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('login', 'AuthJwt\AuthController@login');
+    Route::post('logout', 'AuthJwt\AuthController@logout');
+    Route::post('refresh', 'AuthJwt\AuthController@refresh');
+    Route::post('me', 'AuthJwt\AuthController@me');
+
+});
+
 
 Route::apiResource('shipments',\Shipments\ShipmentController::class);
 
