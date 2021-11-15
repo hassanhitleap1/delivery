92f936f2e4630619e5ccf16305298447d9d650c6
hasan kiwan <hassanhitleap2@gmail.com>
2021-11-15 12:29:53 +0200

    11-15-2021

---
+++ resources/js/_App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <Navbar />
+        <Sidebar />
+        <div class="content-wrapper">
+            <Contentheader />
+            <router-view></router-view>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script>
+    import  Navbar from "./components/Navbar";
+    import  Sidebar from "./components/Sidebar";
+    import  Footer from "./components/Footer";
+    import Contentheader from "./components/Contentheader";
+
+    export default {
+        name: "App",
+        components:{
+            Contentheader,
+            Navbar,Sidebar,Footer
+        }
+
+    };
+</script>
+
